ab79c430d2f2664374340e339a0d9fb4e5e72b5f
gleidsonteixeira <gleidsondeveloper@gmail.com>
2025-05-13 17:10:09 -0300

    inicio de projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import logo from "../assets/logo.png";
+import lupa from "../assets/icon-lupa.png";
+import carrinho from "../assets/icon-carrinho.png";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="topo">
+                <div className="logo">
+                    <img src={logo} alt="Digital store logo" />
+                    DIGITAL STORE
+                </div>
+                <div className="buscador">
+                    <input type="text" placeholder="Pesquisar produto..." />
+                    <img src={lupa} alt="lupa" />
+                </div>
+                <div className="acoes">
+                    <a href="">Cadastre-se</a>
+                    <a href="" className="btn">Entrar</a>
+                    <div className="carrinho">
+                        <img src={carrinho} alt="carrinho" />
+                        <span>2</span>
+                    </div>
+                </div>
+            </div>
+            <nav>
+                <a href="" className="active">Home</a>
+                <a href="">Produtos</a>
+                <a href="">Categorias</a>
+                <a href="">Meus Pedidos</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
